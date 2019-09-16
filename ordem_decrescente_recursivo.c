ecc46966ef241eea32788a803de3fa0e7b106515
Giordano Silveira <ghs19@inf.ufpr.br>
2019-09-15 22:57:14 -0300

    Ordem crescente recursivo

---
+++ ordem_decrescente_recursivo.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+void decrescente_recursivo (int n) {
+
+	if (n>=1) {
+		printf ("%d ", n);
+		return decrescente_recursivo (n-1);
+	}
+}
+
+int main (void) {
+
+	int n;
+
+	scanf ("%d", &n);
+
+	decrescente_recursivo (n);
+	printf ("\n");
+
+	return 0;
+}
