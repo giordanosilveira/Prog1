dfd79b128bede1e61cc2a19002373ba47513d2ac
Giordano Silveira <ghs19@inf.ufpr.br>
2019-09-19 10:30:16 -0300

    arrumar o selection

--- c_programs/selection_recursivo.c
+++ c_programs/selection_recursivo.c
@@ -1,57 +1,56 @@
 #include<stdio.h>
 #define MAX 100
 
-int achamax (int *v, int t, int m) {
-
+int achamax (int *v, int t, int *m) {	
 	if ( t >= 0 ) {
-		if ( v[m] < v[t-1] ) {
-			m=t-1
-			achamax (v,t-1,m)
-		}
+		if ( v[*m] < v[t] ) 
+			*m=t;
+		achamax (v,t-1,m);
 	}
-	return m;
+	return *m;
 		
 }
-//void selection_sort (int *v; int t; int m); {
+void selection_sort (int *v, int t, int m); {
 
-//	if (m < v[t-1]) and (t>=0) {
-//		m=v[t-1];
-//		selection_sort (v,t--,m);
-//	}
-//	else
-//		selection_sort (v,t-i,m);
+	if (m < v[t-1]) and (t>=0) {
+		m=v[t-1];
+		selection_sort (v,t--,m);
+	}
+	else
+		selection_sort (v,t-i,m);
 
-//}
+}
 
 	
-void ler_vetor (int * v; int t) {
+void ler_vetor (int * v, int t) {
 
 	int i;
 
-	for (i=0; i<t ; i++); 
+	for (i=0;i<t;i++) 
 		scanf ("%d", &v[i]);
 }
 
-void escrever_vetor (int * v; int t){
+void escrever_vetor (int * v, int t){
 
 	int i;
 
-	for (i=0; i<t; i++);
+	for (i=0; i<t; i++)
 		printf ("%d ", v[i]);
 	printf ("\n");
 
 }	 
 int main (void) {
 
-	int vetor[MAX], tam, max;
+	int vetor[MAX], tam, maior, max;
 
 	scanf ("%d", &tam);
 
 	ler_vetor (vetor,tam);
+	max=tam-1;
 
-	max = achamax (vetor,tam,tam);
+	maior = achamax (vetor,tam-2,&max);
+	printf ("O posição do maior é: %d\n", max );
 	
-	printf ("%d", max );
 	//selection_sort (vetor,tam,v[tam]);
 
 	escrever_vetor (vetor,tam);
