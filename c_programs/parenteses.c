e071e12a808bc1c758afbc2d79633f5dde638ee4
Giordano Silveira <ghs19@inf.ufpr.br>
2019-09-21 21:56:13 -0300

    balanced parentheses

--- c_programs/parenteses.c
+++ c_programs/parenteses.c
@@ -1,46 +1,46 @@
 #include <stdio.h>
 #include "tad_pilha.h"
-int comparebracket (char open, char close) {
-	if ( (open == '(' ) && (close == ')' ) ) return 1;
-	else if ( (open == '[' ) && (close == ']' ) ) return 1;
-	else ( (open == '{' ) && (close == '}' ) ); return 1;
+
+int comparaparenteses (char open, char close) {
+	if  (open == '(' && close == ')' )  return 1;
+	else if ( open == '['  && close == ']'  ) return 1;
+	else if ( open == '{'  && close == '}' ) return 1;
 	return 0;
 }
 int main () {
 
 	char x,y;
-	int t;
 	tad_pilha p;
 
 	inicializa_pilha (&p);
 	
-	scanf ("%c",&x);
-	empilha (x,&p);
-
+	scanf ("%c %c",&x, &y);	
+	if ( comparaparenteses (x,y) != 1 ) {
+		empilha (x,&p);
+		empilha (y,&p);
+	}
+	scanf (" %c",&x);
 	while (x != '0') {
-		scanf ("%c", &x);
-		if (desempilha (&y,&p) == 1 ) {
-			printf ("aqui porra %c %c\n", y,x);
-			if (topo(&t,&p) == 1)
-				printf ("%d", t);
-			if (comparebracket(y,x) != 1 )	{
-				printf ("aqui porra\n");
-				empilha (y,&p);
+		if (pilha_vazia(&p) == 1) {
+			scanf (" %c",&y);
+			if (comparaparenteses(x,y) != 1) {
 				empilha (x,&p);
+				empilha (y,&p);
 			}
 		}
 		else {
-			empilha (x,&p);
-			scanf ("%c", &y);
-			if (desempilha (&x,&p) == 1){		 
-				if (comparebracket(x,y) != 1 )	{
-					printf ("aqui porra\n");
+			if (desempilha (&y,&p) == 1)
+				if (comparaparenteses(y,x) != 1){
 					empilha (y,&p);
 					empilha (x,&p);
 				}
-			}
 		}
+		scanf (" %c", &x);
 	}
+	if (pilha_vazia(&p) == 1)
+		printf ("SIM\n");
+	else
+		printf ("NÃO\n");
 	return 0;	
 }
 
