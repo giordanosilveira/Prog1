1d69d7414055254650389a99da936dafeaa79c21
GIORDANO HENRIQUE SILVEIRA <ghs19@urquell>
2019-10-11 14:27:31 -0300

    multiplica_matriz v2

--- c_programs/programacao1/multiplica_matrizesV2.c
+++ c_programs/programacao1/multiplica_matrizesV2.c
@@ -11,7 +11,7 @@ int ** aloca_matriz (T_matriz mat) {
 	int i;
 	int ** mat_aux;
 
-	mat_aux= (int**)malloc (sizeof(int)*mat.linha);
+	mat_aux= (int**)malloc (sizeof(int*)*mat.linha);
 	for (i=0;i<mat.linha;i++)
 		mat_aux[i]=(int *)malloc (sizeof(int) * mat.coluna);
 	return mat_aux;
@@ -36,34 +36,55 @@ void imprimir_resultado (T_matriz mat) {
 		printf ("\n");
 	}
 }
+void multiplica_matrizes (T_matriz mat1, T_matriz mat2, T_matriz *mat3) {
 
+	int i,j,k,soma;
+	
+	soma=0;
+	for (i = 0;i < mat1.linha;i++) {
+		for (j=0;i < mat1.coluna;j++){
+			for (k=0; k < mat2.linha; k++)
+				soma = soma + mat1.matriz[i][j]*mat2.matriz[k][j];
+			mat3->matriz[i][j]=soma;	
+		}
+	}
+}
 int main () {
 
 	T_matriz mat1, mat2, mat3;
 
 	printf ("Entre com o números das linhas e colunas da primeira matriz\n");
 	scanf ("%d %d", &mat1.linha, &mat1.coluna);
-	mat1.matriz = aloca_matriz (mat1);
 
 	printf ("Entre com os números das linhas e colunas da segunda matriz\n");
 	scanf ("%d %d", &mat2.linha, &mat2.coluna);	
-	mat2.matriz = aloca_matriz (mat2);
 
-	printf ("matriz1\n");
-	ler_matriz (&mat1);
-
-	printf ("matriz2\n");
-	ler_matriz (&mat2);
-
-	/*if (mat2.matriz != NULL && mat1.matriz != NULL) {
-
-		if (multiplica_matriz (mat1,mat2,mat3))
-			imprime_resultado (mat3);
-		else
-			printf ("NÃO DEU CERTO");
+	if (mat1.coluna != mat2.linha) {
+		printf ("Matrizes inválidas\n");
 	}
-	else
-		printf ("MEMORIA CHEIA BRO");*/
+	else {
+		mat1.matriz = aloca_matriz (mat1);
+		mat2.matriz = aloca_matriz (mat2);
 	
+		if ((mat2.matriz != NULL) && ((mat1.matriz) != NULL)) {
+			printf ("matriz 1 !?\n");
+			ler_matriz (&mat1);
+
+			printf ("matriz 2 !?\n");
+			ler_matriz (&mat2);
+
+			mat3.linha = mat1.linha; mat3.coluna=mat2.coluna;
+			mat3.matriz = aloca_matriz (mat3);
+				if (mat3.matriz != NULL){
+					printf ("Aqui");
+					multiplica_matrizes (mat1,mat2,&mat3);
+					imprimir_resultado (mat3);
+				}
+				else
+					printf ("MEMORIA CHEIA BRO");
+		}
+		else
+			printf ("MEMORIA CHEIA BRO");
+	}
 	return 0;
 }
