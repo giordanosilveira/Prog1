8d4801bc93697eb18c5eb35d1899c1c66ea39e60
Giordano Silveira <ghs19@inf.ufpr.br>
2019-10-25 00:23:02 -0300

    Funções lista feitas, porém não compiladas, é isso

--- c_programs/programacao1/linkedlist/lista.c
+++ c_programs/programacao1/linkedlist/lista.c
@@ -1,4 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lista.h"
 
+void imprime_lista (t_lista *l) {
+	
+	t_nodo *p;
+
+	if (lista_vazia(l))
+		printf ("Lista vazia\n");
+	else {
+		p = l->ini;
+		printf ("O tamanho é %d", l->tam);
+		while (p->prox != NULL) {
+			printf ("%d ", p->elemento);
+			p = p->prox;
+		}
+		printf ("\n");
+	}
+
+
+}
 int cria_lista(t_lista *l){
 	l->ini = NULL;
 	l->tam = 0;
@@ -12,6 +33,22 @@ int lista_vazia(t_lista *l) {
 }
 
 void destroi_lista(t_lista *l) {
+
+	t_nodo *q, *p;
+	
+	if (lista_vazia(l))
+		printf ("Lista vazia\n");
+	else {
+		p=l->ini;
+		while (p->prox != NULL) {
+			l->ini = p->prox;
+			free (p);
+			p = p->prox;
+		}
+		free (p);
+		l->tam = 0;
+		l->ini = NULL;
+	}
 }
 
 int insere_inicio_lista(int x, t_lista *l) {
@@ -38,7 +75,7 @@ int insere_fim_lista(int x, t_lista *l) {
 	}
 
 	new=(t_nodo *) malloc (sizeof(t_nodo));
-	if (new==NULL)
+	if (new==NULL)		
 		return 0;
 
 	l->tam++;
@@ -47,7 +84,8 @@ int insere_fim_lista(int x, t_lista *l) {
 	p = l->ini;
 	while (p->prox != NULL) {
 		p = p->prox;
-	}	
+	}
+	new->elemento = x;	
 	p->prox=new;
 }
 
@@ -56,31 +94,40 @@ int insere_ordenado_lista(int x, t_lista *l) {
 	if (lista_vazia(l))
 		return insere_inicio_lista (x,l);
 
-	t_nodo *new;
+	t_nodo *new, *ant, *p;
 
 	/*se a lista tem só um elemento*/
 	if (l->ini->prox == NULL) {
-		if (x > l->ini->chave)
+		if (x > l->ini->elemento)
 			return insere_fim_lista (x,l);
 		else
 			return insere_inicio_lista (x,l);
 	}
 	
 	/*se a lista não tem nenhum elemento*/
-	if (l->ini == NULL)
+	if (l->ini->chave > x)
 		return insere_inicio_lista (x,l);
 
 	/*caso geral*/
 	
-	new=(t_nodo *)malloc(sizeof(t_nodo))
-	if (new == NULL)
-		return 0;
 
-	new=l->ini;
-	while (new->prox != NULL && x < new->chave)
-		new = new->prox;
+	p=l->ini;
+	while (p->prox != NULL && x < p->elemento){
+		ant = p;
+		p = p->prox;
+	}
 
-	if (x => new->chave)
+	if (x => p->elemento) {
+		new=(t_nodo *)malloc(sizeof(t_nodo))
+		if (new == NULL)
+			return 0;
+		new->elemento = x;
+		new->prox = p;
+		q->prox = new;
+		l->tam++;
+		return 1;
+	}
+	return 0;
 		
 } 	
 int remove_primeiro_lista(int *item, t_lista *l) {
@@ -89,20 +136,12 @@ int remove_primeiro_lista(int *item, t_lista *l) {
 	if (lista_vazia(l))
 		return 0;
 	
-	*item = l->ini->chave;
+	*item = l->ini->elemento;
 	l->tam--;
 
-	/*caso especial se a lista tem só um elemento*/
-	if (l->ini->prox == NULL) {
-		free(l->ini);
-		l->ini = NULL;
-		return 1;
-	}
-	
+	free(l->ini);
 	l->ini = l->ini->prox;
 	return 1;
-	
-	
 }
 int remove_ultimo_lista(int *item, t_lista *l) {
 
@@ -114,9 +153,6 @@ int remove_ultimo_lista(int *item, t_lista *l) {
 	l->tam--;
 
 	/*se a lista tem somente um elemento*/
-	p=(t_nodo *)malloc(sizeof(t_nodo));
-	if (p == NULL)
-		return 0;
 
 	if (l->ini->prox == NULL){
 		return remove_primeiro_lista (item,l);
@@ -131,64 +167,61 @@ int remove_ultimo_lista(int *item, t_lista *l) {
 	p->prox=NULL;
 	return 1;
 }
-int remove_item_lista(int chave, int *item, t_lista *l) {
+int remove_item_lista(int elemento, int *item, t_lista *l) {
 
 	if (lista_vazia(l))
 		return 0;
 	
 	t_nodo *p, *q;
-	if (l->ini->chave == chave) {
+	if (l->ini->elemento == elemento) {
 		l->tam--;
 		return (remove_primeiro_lista(item,l));
 	}
 	
 	/*caso geral*/
 	p = l->ini;
-	while (p->prox != NULL && p->chave != chave){
+	while (p->prox != NULL && p->elemento != elemento){
 		q = p;
 		p = p->prox;
 	}
-	if (p->chave == chave){
+	if (p->elemento == elemento){
 		l->tam--;
 		q->prox = p->prox;
 		free (p);
+		return 1;
 	}
 	printf ("Elemento não encontrado\n");
 	return 0;
 }
-int pertence_lista(int chave, t_lista *l) {
+int pertence_lista(int elemento, t_lista *l) {
 
 	if (lista_vazia(l))
 		return 0;
 	
 	t_nodo *p;
 	p = l->ini;
-	while (p->prox != NULL && p->chave != chave)
+	while (p->prox != NULL && p->elemento != elemento)
 		p = p->prox;
-	if (p->chave == chave)
+	if (p->elemento == elemento)
 		return 1;
-	printf ("Elemento não encontrado");
+	printf ("Elemento não encontrado\n");
+	return 0;
 
 }
 int concatena_listas(t_lista *l, t_lista *m) {
 
-	if (lista_vazia(l)) {
-		l->ini = m->ini;
-		m->ini = NULL;
-		l->tam = m->tam; 
-		cria_lista (m);
-		return 1;
+	if (lista_vazia(l) && lista_vazia (m)) {
+		return 0;
 	}
 	
 	/*caso geral*/
 	t_nodo *p;
-	p = p->ini;
-	while (p->prox != NULL)
+	p = m->ini;
+	while (p->prox != NULL) {
+		insere_fim_lista (p->elemento,l);
 		p = p->prox;
-	p->prox = m->ini;
-	m->ini = NULL;
-	l->tam = l->tam + m->tam;
-	cria_lista (m);
+	}
+	destroi_lista(m);
 	return 1;
 }
 int copia_lista(t_lista *l, t_lista *m) {
@@ -202,9 +235,9 @@ int copia_lista(t_lista *l, t_lista *m) {
 
 	p = l->ini;
 	while (p->prox != NULL) {
-		insere_fim_lista (p->chave,m);
+		insere_fim_lista (p->elemento,m);
 		p = p->prox;
 	}
-	insere_fim_lista (p->chave,m);
+	insere_fim_lista (p->elemento,m);
 }
 
