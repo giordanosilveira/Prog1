fc6a4f994c5979f58977ff0932984d06e12f39e5
GIORDANO HENRIQUE SILVEIRA <ghs19@urquell>
2019-10-30 14:50:55 -0300

    Output do main.c

---
+++ c_programs/programacao1/JorginhosEletronicos/space_invaders/invasoresdoespaco.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ncurses.h>
+#include <unisted.h>
+
+#define ET11 "/*-*\\"
+#define ET12 "|/O\\|"
+#define ET13 "\\/-\\/"
+#define ET112 "/*-*\\"
+#define ET122 "_/O\\_"
+#define ET132 "\\/|\\/"
+
+#define ET21 "|O.O|"
+#define ET22 ")_O_("
+#define ET23 "V*+*V"
+#define ET212 "/o.o\\"
+#define ET222 "/|_|\\"
+#define ET223 "(_O_)"
+
+#define ET31 
+#define ET32
+#define ET33
+#define ET312 
+#define ET322
+#define ET332
+int main () {
+
+	/* coisas do moodle...*/
+	initscr(); 
+    	cbreak();           
+    	noecho();            
+    	nodelay(stdscr, TRUE);
+   	keypad(stdscr, TRUE);  
+    	curs_set(FALSE);
+
+	
+
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
