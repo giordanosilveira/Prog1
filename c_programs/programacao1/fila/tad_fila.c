bf7647abfe4f02d651a72d1eb77a04a6d8216e81
Giordano Silveira <ghs19@inf.ufpr.br>
2019-11-12 00:01:44 -0300

    Não deu tempo de terminar a fila, puts que pena

--- c_programs/programacao1/fila/tad_fila.c
+++ c_programs/programacao1/fila/tad_fila.c
@@ -2,20 +2,28 @@
 #include <stdlib.h>
 #include "tad_fila.h"
 
-void imprime_lista (t_lista *l) {
+void imprime_fila (t_fila *l) {
 
-	t_nodo *aux;
-	if (lista_vazia(l) != 1) {
-		aux = l->ini->prox;
-		while (aux->prox != l->fim) {
-			printf ("%d ", aux->chave);
-			aux = aux->prox;
+	int i, item, tamanho, tempo;
+
+	tamanho = tamanho_fila (f);
+
+	if (fila_vazia(f))
+		return;
+
+	inicializa_atual_inicio (f);
+
+	for (i = 0; i < tamanho; i++) {
+		consulta_id_atual (&item,f);
+		consulta_tempo_atual (&tempo,f);
+		if (versao) {
+			printf ("%d ", item);
 		}
-		printf ("%d ", aux->chave);
-		printf ("\n");
-	}
+		else 
+			printf ("%d(%d) ", item, tempo);
+		incrementa_atual(f);
 }
-int inicializa_lista(t_lista *l) {
+int inicializa_fila(t_fila *l) {
 
 	t_nodo *ini, *fim;
 	
@@ -45,18 +53,26 @@ int inicializa_lista(t_lista *l) {
 	return 1;
 }
 
-int lista_vazia(t_lista *l) {
+int fila_vazia(t_fila *l) {
 
 	if (l->tamanho == 0)
 		return 1;
 	return 0;
 }
-void destroi_lista(t_lista *l) {
+void destroi_fila(t_fila *l) {
 
+	int lixo, i, tam;
+
+	tam = f->tamanho;
+
+	for (i = 0, i < tam; i++)
+		desenfileira (&lixo, f);
+
+	if
 	t_nodo *aux;
 
 	aux = l->ini->prox;
-	if (! lista_vazia(l)) {
+	if (! fila_vazia(l)) {
 		while (aux->prox != NULL) {
 			aux = aux->prox;
 			free (aux->prev);
@@ -66,12 +82,13 @@ void destroi_lista(t_lista *l) {
 	free (l->fim);
 	free (l->ini);
 }
-int tamanho_lista(int *tam, t_lista *l) {
+int tamanho_fila(int *tam, t_fila *l) {
 
-	*tam = l->tamanho;
-	return 1;
+	if (f->tamanho == 0)
+		return f->tamanho;
+	return -1;
 }
-int insere_fim_lista(int item, t_lista *l){
+int enfileira (int id, int tempo, t_fila *l){
 
 	t_nodo *new;
 
@@ -80,8 +97,10 @@ int insere_fim_lista(int item, t_lista *l){
 		return 0;
 
 	l->tamanho++;
+	
+	new->id = id;
+	new->tempo = tempo;
 
-	new->chave = item;
 	new->prox = l->fim;
 	new->prev = l->fim->prev;
 
@@ -90,26 +109,33 @@ int insere_fim_lista(int item, t_lista *l){
 
 	return 1;
 }
-int remove_inicio_lista(int *item, t_lista *l) {
+int desenfileira (int *item, t_fila *l) {
 	
-	if (lista_vazia (l))
+	if (fila_vazia (l))
 		return 0;
 
-	*item = l->ini->prox->chave;
+	f->fim->id  = id;
 
-	l->ini->prox = l->ini->prox->prox;
-	free (l->ini->prox->prev);
-	l->ini->prox->prev = l->ini;
+	p = f->ini;
+	while (p->prox->id != id)	
+		p = p->prox;
+	
+	if (p == f->fim->prev)
+		return 0;
 
-	l->tamanho--;
+	p->prox = p->prox->prev;
+	free (p->prox->prev);
+	p->prox->prev = p;
+
+	f->tamanho--;
 	
 	return 1;
 }
-int remove_fim_lista(int *item, t_lista *l) {
+int remove_fim_fila(int *item, t_fila *l) {
 
 	t_nodo *aux;
 	
-	if (lista_vazia (l))
+	if (fila_vazia (l))
 		return 0;
 
 	*item = l->fim->prev->chave;
@@ -125,10 +151,10 @@ int remove_fim_lista(int *item, t_lista *l) {
 
 	return 1;
 }
-int remove_item_lista(int chave, int *item, t_lista *l) {
+int remove_item_fila(int chave, int *item, t_fila *l) {
 
 
-	if (lista_vazia(l))
+	if (fila_vazia(l))
 		return 0;
 
 	l->fim->chave = chave;
@@ -149,11 +175,11 @@ int remove_item_lista(int chave, int *item, t_lista *l) {
 	l->tamanho--;
 	return 1;
 }
-int pertence_lista(int chave, t_lista *l) {
+int pertence_fila(int chave, t_fila *l) {
 
 	t_nodo *aux;
 	
-	if (lista_vazia (l))
+	if (fila_vazia (l))
 		return 0;
 
 	aux = l->ini->prox;
@@ -166,27 +192,27 @@ int pertence_lista(int chave, t_lista *l) {
 		return 0;
 	return 1;
 }
-int inicializa_atual_inicio(t_lista *l) {
+int inicializa_atual_inicio(t_fila *l) {
 
-	if (lista_vazia(l))
+	if (fila_vazia(l))
 		return 0;
 
 	l->atual = l->ini->prox;
 	return 1;
 }
-int inicializa_atual_fim(t_lista *l) {
+int inicializa_atual_fim(t_fila *l) {
 
-	if (lista_vazia(l))
+	if (fila_vazia(l))
 		return 0;
 
 	l->atual = l->fim->prev;
 	return 1;
 
 }
-int incrementa_atual(t_lista *l) {
+int incrementa_atual(t_fila *l) {
 
-	/*se a lista não estiver vazia*/
-	if (lista_vazia(l)) return 0;
+	/*se a fila não estiver vazia*/
+	if (fila_vazia(l)) return 0;
 	
 	if (l->atual == l->fim->prev) return 0;
 
@@ -194,10 +220,10 @@ int incrementa_atual(t_lista *l) {
 	
 	return 1;
 }
-int decrementa_atual(t_lista *l) {
+int decrementa_atual(t_fila *l) {
 
-	/*se a lista não estiver vazia*/
-	if (lista_vazia(l)) return 0;
+	/*se a fila não estiver vazia*/
+	if (fila_vazia(l)) return 0;
 
 	if (l->atual == l->ini->prox) return 0;
 
@@ -205,19 +231,19 @@ int decrementa_atual(t_lista *l) {
 
 	return 1;
 }
-int consulta_item_atual(int *item, t_lista *l) {
+int consulta_item_atual(int *item, t_fila *l) {
 
-	if (lista_vazia(l))
+	if (fila_vazia(l))
 		return 0;
 
 	*item = l->atual->chave;
 	return 1;
 }
-int remove_item_atual(int *item, t_lista *l) {
+int remove_item_atual(int *item, t_fila *l) {
 
 	t_nodo *p;
 
-	if (lista_vazia(l))
+	if (fila_vazia(l))
 		return 0;
 
 	*item = l->atual->chave;
